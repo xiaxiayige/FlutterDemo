5dcce1ae238c494114137aa0b0dbdd2b9431fb3c
zhangqilin <xiaxiayige@163.com>
2020-12-03 11:02:58 +0800

    添加启动图片，修改名称

--- flutter_pan_baidu/lib/main.dart
+++ flutter_pan_baidu/lib/main.dart
@@ -48,6 +48,7 @@ class MyApp extends StatelessWidget {
     ));
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
 //        scaffoldBackgroundColor: Colors.white
